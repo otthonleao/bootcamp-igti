a0bea9917b5bbf4e9fadd39099695c44b5719a53
Otthon Leão <otthonleao@hotmail.com>
2020-05-24 16:59:11 -0400

    upgrade

--- Módulo 1/aula09-crud/js/script.js
+++ Módulo 1/aula09-crud/js/script.js
@@ -2,103 +2,43 @@
 window.addEventListener('load', start);
 
 var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
-var inputName = null;
-var currentIndex = null;
-var isEditing = false;
+var inputName = null; // Só declarei a variável pq não posso garantir que a página está carregada. Por isso declaro na função 'start()'
 
 function start() {
-  inputName = document.querySelector('#input');
+  console.log('start')
   preventFormSubmit();
-  activateInput();
-  render();
+  activeInput();
+  
 }
 
+// Impedir que a página recarregue após o 'enter'
 function preventFormSubmit() {
-  function handleFormSubmit(event) {
-    event.preventDefault();
-  }
   var form = document.querySelector('form');
-  form.addEventListener('submit', handleFormSubmit);
+  form.addEventListener('submit', handleFormSubmit) // O evento acontece quando submeto o dado
+    function handleFormSubmit(event) {
+      event.preventDefault(); // preventDefault() impede que o evento padrão ocorra
+  }
 }
 
-function activateInput() {
-  function insertName(newName) {
-    globalNames.push(newName);
-    console.log(globalNames);
-    render();
-  }
+// Todas as ativações no input 
+function activeInput() {
+  inputName = document.querySelector('#inputName');
+  inputName.focus() // Ativar o pisca de digitação no input quando a página for carregada
 
-  function updateName(newName) {
-    globalNames[currentIndex] = NewName
-    render();
-  }
-
-  function handleTyping(event) {
-    var hasText
-    if (event.key === 'Enter') {
-      if (isEditing) {
-        updateName(event.target.value);
-      } else {
-        insertName(event.target.value);
+  // Inserção de novos nomes no vetor 'globalNames'
+  inputName.addEventListener('keyup', handleTyping); // 'keyup' identifica quando soltamos o teclado após apertar uma tecla.
+    function handleTyping(event) {
+      if(event.key === 'Enter') { // Se a tecla identificada for 'Enter'
+        insertName(event.target.value); // Inserir o valor que foi digital no alvo (no input)
+          function insertName(NewName) {
+            globalNames.push(NewName); // Insere um novo valor/nome na array globalNames após o Enter
+            console.log(globalNames);
+          }
       }
-      isEditing = false;
-      clearInput();
     }
-  }
-  inputName.addEventListener('keyup', handleTyping);
-  inputName.focus(); //Coloca oc cursor de digitação piscando
 }
 
+// Após inserir o valor, manipular o #names para inserir dinamicamente dentro da div
 function render() {
-  function createDeleteButton(index) {
-    function deleteName() {
-      globalNames.splice(index, 1);
-      render();
-    }
-    var button = document.createElement('button');
-    button.classList.add('deleteButton');
-    button.textContent = 'x';
-    button.addEventListener('Click', deleteName);
-    return button;
-  }
-
-  function createSpan(name, index) {
-    function editItem() {
-      inputName.value = name;
-      inputName.focus();
-      isEditing = true;
-      currentIndex = index;
-    }
-    var span = document.querySelector('span');
-    span.classList.add('clickable');
-    span.textContent = name;
-    span.addEventListener('click', editItem);
-    return span;
-  }
-
-  var divNames = document.querySelector('#names');
-  divNames.innerHTML = '' // Para limpar o input após o enter
-
-  var ul = document.createElement('ul');
-
-  for (var i = 0; i < globalNames.length; i++) {
-    var currentName = globalNames[i];
-
-    var li = document.createElement('li');
-    var button = document.createElement('button');
-    var span = createSpan(currentName);
-    button.textContent = 'x';
-
-    var span = document.createElement('span');
-    span.textContent = currentName;
-    li.textContent = currentName;
-    ul.appendChild(li);
-  }
-  divNames.appendChild(ul);
-  clearInput();
-}
-
-function clearInput(){
-  inputName.value = '';
-  inputName.focus();
+  
 }
